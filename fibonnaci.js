0c39ba93204c053a0128ad662e6ecf82e6adb641
Tosin Aremu <66263731+Tohsyne@users.noreply.github.com>
2020-08-13 03:37:21 -0700

    Add files via upload

---
+++ fibonnaci.js
@@ -0,0 +1,14 @@
+    var var1= 0;
+    var var2 = 1;
+    var var3;
+    var num= prompt ("Enter the number limit to the fibonacci sequence", 0);
+    document.write(var1 + "<br />");
+    document.write(var2 + "<br />");
+    for (var i=3; i<=num; i++)
+    {
+        var3 = var1 + var2;
+        var1 = var2;
+        var2 = var3;
+        document.write(var3 + "<br />");
+       }
+   
